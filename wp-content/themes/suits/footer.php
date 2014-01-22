fb699394092a7ecbda4e99d82d17af0b083b520e
John Kirkpatrick <jkirk@me.com>
2014-01-22 10:50:15 -0500

    Footer deletion and CSS responsive header test

--- wp-content/themes/suits/footer.php
+++ wp-content/themes/suits/footer.php
@@ -15,12 +15,6 @@
 			<?php get_sidebar( 'footer' ); ?>
 
 			<div class="site-info-container">
-				<div class="site-info">
-					<?php do_action( 'suits_credits' ); ?>
-					<?php printf( __( 'Proudly powered by %s', 'suits' ), '<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform">WordPress</a>' ); ?>
-					<span class="sep"> &middot; </span>
-					<?php printf( __( 'Theme: %1$s by %2$s', 'suits' ), 'Suits', '<a href="http://www.themeweaver.net/" title="Theme Developer" rel="designer">Theme Weaver</a>' ); ?>
-				</div><!-- .site-info -->
 			</div><!-- .site-info-container -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
